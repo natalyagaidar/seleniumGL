19727838c195003eced5beb3c97f91bb07a7c455
nataliia.gaidar <natalyagaidar@gmail.com>
2017-04-20 08:40:24 +0300

    Task 5 - Deleted duplicated line

--- src/test/java/Task5.java
+++ src/test/java/Task5.java
@@ -25,7 +25,6 @@ public class Task5 {
         driver1 = new ChromeDriver();
 
         driver1.get("http://localhost/litecart/admin/");
-        driver1.get("http://localhost/litecart/admin/");
         driver1.findElement(By.name("username")).sendKeys("admin");
         driver1.findElement(By.name("password")).sendKeys("admin");
         driver1.findElement(By.name("remember_me")).isEnabled();
